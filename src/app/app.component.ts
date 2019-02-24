cc8bd7343a0fd766ec0d6537b2fee5fa03415aa3
Will <doggy.huang@gmail.com>
2019-02-24 16:54:46 +0800

    示範如何直接刪除 JS 陣列中的文章資料 (強型別)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { from } from 'rxjs';
+import { Article } from './article';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,7 @@ export class AppComponent implements OnInit {
   title = 'demo1';
   keyword = '';
 
-  data = [];
+  data: Article[] = [];
 
   ngOnInit(): void {
     from(fetch('/api/articles.json').then(res => res.json()))
@@ -33,5 +34,6 @@ export class AppComponent implements OnInit {
 
   deleteArticle(postId: number) {
     console.log(`你刪除編號 ${postId} 的文章!`);
+    this.data = this.data.filter(a => a.id !== postId);
   }
 }
